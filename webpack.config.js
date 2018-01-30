4282366ab3a24185ab33504b82c0746cfc2a9992
Adam Shaw <arshaw@users.noreply.github.com>
2018-01-30 09:47:43 -0500

    rely on some browser globals

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,13 @@ module.exports = {
     filename: 'example.js',
     path: path.join(__dirname, 'dist')
   },
+  externals: {
+    // shows how we can rely on browser globals instead of bundling these dependencies,
+    // in case we want to access jQuery from a CDN or if we want an easy way to
+    // avoid loading all moment locales: https://github.com/moment/moment/issues/1435
+    jquery: 'jQuery',
+    moment: 'moment'
+  },
   devtool: 'sourcemap',
   resolve: {
     extensions: [ '.ts', '.js' ],
