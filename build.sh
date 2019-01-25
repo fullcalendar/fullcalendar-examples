60438a0bf0d0fca887eb55f2a811aab085be46df
Adam Shaw <arshaw@users.noreply.github.com>
2019-01-25 16:15:15 -0300

    build script

---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# always immediately exit upon error
+set -e
+
+# start in root
+cd "`dirname $0`"
+
+root_dir="$PWD"
+
+production_str="$1"
+
+if [[ "$production_str" == "" ]]
+then
+  echo "Will install from DEV NPM INSTALLATION."
+  npm_registry_str="--registry http://localhost:4873"
+elif [[ "$production_str" == "--production" ]]
+then
+  echo "Will install from LIVE NPM."
+  npm_registry_str=""
+else
+  echo "Invalid flag $production_str. Aborting"
+  exit 1
+fi
+
+for package_file in */package.json
+do
+  cd "$(dirname $package_file)"
+
+  echo "*"
+  echo "* Repo: $PWD"
+  echo "*"
+
+  echo "Clearing..."
+  rm -rf node_modules package-lock.json
+
+  echo "Installing..."
+  npm install $npm_registry_str
+
+  echo "Building..."
+  npm run build
+
+  echo "Opening browser..."
+
+  for html_file in *.html
+  do
+    open -a "Google Chrome" "file://$PWD/$html_file"
+  done
+
+  cd "$root_dir"
+done
+
+echo "*"
+echo "* Success!"
+echo "*"
