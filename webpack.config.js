302107b4607c8cf64fcf35085ac2f804264cf604
Adam Shaw <arshaw@users.noreply.github.com>
2019-03-31 13:31:47 -0500

    simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,7 @@
-const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: path.join(__dirname, 'src/index.js'),
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'main.js'
-  },
-  optimization: {
-    // We no not want to minimize our code.
-    minimize: false
-  },
+  entry: './src/index.js',
   module: {
     rules: [
       {
