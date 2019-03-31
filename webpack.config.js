8b1622efe91c7ddeffa4bbf70b8b2fdb1f49cf76
Adam Shaw <arshaw@users.noreply.github.com>
2019-03-31 13:48:59 -0500

    more cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader' // will use .babelrc
         }
       },
       {
