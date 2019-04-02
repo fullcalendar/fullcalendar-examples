629d877b67364a6277fa63e2f197a4f89af43a54
Adam Shaw <arshaw@users.noreply.github.com>
2019-04-02 17:20:00 -0500

    improvements

--- react/src/DemoApp.jsx
+++ react/src/DemoApp.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import FullCalendar from 'sardius-fullcalendar-wrapper'
+import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction' // needed for dayClick
 
 import './main.scss'
@@ -42,15 +43,20 @@ export default class DemoApp extends React.Component {
     return (
       <div className='demo-app'>
         <div className='demo-app-top'>
-          <button onClick={ this.toggleWeekends }>dynamically toggle weekends</button>&nbsp;
+          <button onClick={ this.toggleWeekends }>toggle weekends</button>&nbsp;
           <button onClick={ this.gotoPast }>go to a date in the past</button>&nbsp;
           (also, click a date/time to add an event)
         </div>
         <div className='demo-app-calendar'>
           <FullCalendar
-            plugins={[ dayGridPlugin, interactionPlugin ]}
-            ref={ this.calendarComponentRef }
             defaultView="dayGridMonth"
+            header={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+            }}
+            plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
+            ref={ this.calendarComponentRef }
             weekends={ this.state.calendarWeekends }
             events={ this.state.calendarEvents }
             dateClick={ this.handleDateClick }
