1bd3b107de3f04411f90a4b58fc9ecb207dd3413
Adam Shaw <arshaw@users.noreply.github.com>
2019-04-24 12:02:13 -0500

    open angular demo

--- bin/open.sh
+++ bin/open.sh
@@ -12,10 +12,20 @@ for package_file in */package.json
 do
   cd "$(dirname $package_file)"
 
-  for html_file in *.html
-  do
-    open -a "Google Chrome" "file://$PWD/$html_file"
-  done
+  dist_html_files=$(find dist -name '*.html')
+
+  if [[ "$dist_html_files" ]]
+  then
+    for html_file in "$dist_html_files"
+    do
+      open -a "Google Chrome" "file://$PWD/$html_file"
+    done
+  else
+    for html_file in *.html
+    do
+      open -a "Google Chrome" "file://$PWD/$html_file"
+    done
+  fi
 
   cd "$root_dir"
 done
