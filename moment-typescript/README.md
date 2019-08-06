855b09b32e48d12e3e2afa950b7ea2e970beba6d
Adam Shaw <arshaw@users.noreply.github.com>
2019-08-06 14:24:09 -0400

    moment+typescript example

---
+++ moment-typescript/README.md
@@ -0,0 +1,31 @@
+
+# FullCalendar Moment+TypeScript Example Project
+
+This is a fully-buildable example project for FullCalendar and [TypeScript] and [Moment], leveraging [Webpack] and [ts-loader].
+
+[More information on setting up TypeScript with Webpack &raquo;](https://www.typescriptlang.org/docs/handbook/react-&-webpack.html)
+
+
+## Installation
+
+```bash
+git clone https://github.com/fullcalendar/fullcalendar-example-projects.git
+cd fullcalendar-example-projects/moment-typescript
+npm install
+```
+
+## Build Commands
+
+```bash
+npm run build
+npm run watch # continously build
+npm run clean # start fresh
+```
+
+After running `build` or `watch`, open up `index.html` in a browser.
+
+
+[TypeScript]: https://www.typescriptlang.org/
+[Moment]: https://momentjs.com/
+[Webpack]: https://webpack.js.org/
+[ts-loader]: https://github.com/TypeStrong/ts-loader
---
+++ moment-typescript/src/example.ts
@@ -0,0 +1,82 @@
+import { Calendar } from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import momentPlugin from '@fullcalendar/moment';
+
+document.addEventListener('DOMContentLoaded', function() {
+  let calendarEl: HTMLElement = document.getElementById('calendar')!;
+
+  let calendar = new Calendar(calendarEl, {
+    plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin, momentPlugin ],
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    defaultDate: '2018-01-12',
+    titleFormat: 'MMMM [!!!] YYYY', // a moment format string
+    navLinks: true, // can click day/week names to navigate views
+    editable: true,
+    eventLimit: true, // allow "more" link when too many events
+    events: [
+      {
+        title: 'All Day Event',
+        start: '2018-01-01',
+      },
+      {
+        title: 'Long Event',
+        start: '2018-01-07',
+        end: '2018-01-10'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: '2018-01-09T16:00:00'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: '2018-01-16T16:00:00'
+      },
+      {
+        title: 'Conference',
+        start: '2018-01-11',
+        end: '2018-01-13'
+      },
+      {
+        title: 'Meeting',
+        start: '2018-01-12T10:30:00',
+        end: '2018-01-12T12:30:00'
+      },
+      {
+        title: 'Lunch',
+        start: '2018-01-12T12:00:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2018-01-12T14:30:00'
+      },
+      {
+        title: 'Happy Hour',
+        start: '2018-01-12T17:30:00'
+      },
+      {
+        title: 'Dinner',
+        start: '2018-01-12T20:00:00'
+      },
+      {
+        title: 'Birthday Party',
+        start: '2018-01-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2018-01-28'
+      }
+    ]
+  });
+
+  calendar.render();
+});
