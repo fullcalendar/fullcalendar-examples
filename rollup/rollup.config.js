e6433ab2fefc34b39c824a3c5e43ac97fc4753c6
Adam Shaw <arshaw@users.noreply.github.com>
2020-01-21 14:12:37 -0300

    show how to do css vars

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,12 +1,20 @@
 const nodeResolve = require('@rollup/plugin-node-resolve')
 const postcss = require('rollup-plugin-postcss')
+const postcssVariables = require('postcss-css-variables')
 
 module.exports = {
   input: 'src/main.js',
   plugins: [
     nodeResolve(),
     postcss({
-      extract: true // write to dist/main.css
+      extract: true, // write to dist/main.css
+      plugins: [
+        postcssVariables({
+          variables: {
+            '--fc-unthemed-border-color': 'red'
+          }
+        })
+      ]
     })
   ],
   output: {
