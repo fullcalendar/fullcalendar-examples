e367f703d24081f8c4653a27897009103ce2bdc7
Adam Shaw <arshaw@users.noreply.github.com>
2020-01-21 16:22:25 -0300

    webpack example using new css build system

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,13 +1,40 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/example.js',
+  entry: './src/main.js',
   resolve: {
     extensions: [ '.js' ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          { loader: 'postcss-loader', options: {
+            ident: 'postcss',
+            plugins: () => [
+              require('postcss-css-variables')({
+                variables: {
+                  '--fc-unthemed-border-color': 'red'
+                }
+              })
+            ]
+          } }
+        ]
+      }
+    ]
+  },
   output: {
-    filename: 'example.js',
+    filename: 'main.js',
     path: path.join(__dirname, 'dist')
   },
   devtool: 'sourcemap'
