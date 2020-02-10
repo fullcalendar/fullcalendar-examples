550238f63afc35d619d467c7a6e3964b383ea15d
Adam Shaw <arshaw@users.noreply.github.com>
2020-02-10 14:53:49 -0500

    make typescript example work with v5

--- typescript/webpack.config.js
+++ typescript/webpack.config.js
@@ -1,22 +1,35 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/example.ts',
+  entry: './src/main.ts',
   resolve: {
     extensions: [ '.ts', '.js' ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader', options: { importLoaders: 1 } }
+        ]
       }
     ]
   },
   output: {
-    filename: 'example.js',
+    filename: 'main.js',
     path: path.join(__dirname, 'dist')
   },
   devtool: 'sourcemap'
