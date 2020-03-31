2a64a771d976fab7966d1ab32850304ccc147ede
Adam Shaw <arshaw@users.noreply.github.com>
2020-03-30 23:59:02 -0400

    update code to v5 for angular example

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
 import { FullCalendarComponent } from '@fullcalendar/angular';
-import { EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGrigPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction'; // for dateClick
@@ -15,18 +14,28 @@ export class AppComponent {
   @ViewChild('calendar') calendarComponent: FullCalendarComponent; // the #calendar in the template
 
   calendarVisible = true;
-  calendarPlugins = [dayGridPlugin, timeGrigPlugin, interactionPlugin];
-  calendarWeekends = true;
-  calendarEvents: EventInput[] = [
-    { title: 'Event Now', start: new Date() }
-  ];
+  calendarOptions = {
+    plugins: [dayGridPlugin, timeGrigPlugin, interactionPlugin],
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    defaultView: 'dayGridMonth',
+    weekends: true,
+    dateClick: this.handleDateClick.bind(this),
+    events: [
+      { title: 'Event Now', start: new Date() }
+    ] as any
+  };
 
   toggleVisible() {
     this.calendarVisible = !this.calendarVisible;
   }
 
   toggleWeekends() {
-    this.calendarWeekends = !this.calendarWeekends;
+    const { calendarOptions } = this;
+    calendarOptions.weekends = !calendarOptions.weekends;
   }
 
   gotoPast() {
@@ -36,7 +45,11 @@ export class AppComponent {
 
   handleDateClick(arg) {
     if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
-      this.calendarEvents = this.calendarEvents.concat({ // add new event data. must create new array
+      const { calendarOptions } = this;
+
+      // add new event data
+      // must create new array unless using deepChangeDetection
+      calendarOptions.events = calendarOptions.events.concat({
         title: 'New Event',
         start: arg.date,
         allDay: arg.allDay
