72cc8e1ca8e813617971001131b77518c83beef2
Adam Shaw <arshaw@users.noreply.github.com>
2020-05-09 20:53:27 -0400

    rollup example no longer does extract, bc of rollup bug

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -7,7 +7,12 @@ module.exports = {
   plugins: [
     nodeResolve(),
     postcss({
-      extract: true, // write to dist/main.css
+      /*
+      for writing CSS to a separate file (dist/main.css).
+      in rollup v2, this writes CSS rules in wrong order (https://github.com/egoist/rollup-plugin-postcss/issues/96)
+      so, disable for now, and allow the CSS to be embedded in the JS
+      */
+      // extract: true,
       plugins: [
         postcssVariables({
           variables: {
