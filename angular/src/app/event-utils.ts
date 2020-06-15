135de2d53dfc4da74ff731aca2236edb1bf5a124
Adam Shaw <arshaw@users.noreply.github.com>
2020-06-15 00:13:55 -0400

    renames

--- angular/src/app/event-utils.ts
+++ angular/src/app/event-utils.ts
@@ -1,18 +1,18 @@
 import { EventInput } from '@fullcalendar/angular';
 
 let eventGuid = 0;
-const todayStr = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
+const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
 
 export const INITIAL_EVENTS: EventInput[] = [
   {
     id: createEventId(),
     title: 'All-day event',
-    start: todayStr
+    start: TODAY_STR
   },
   {
     id: createEventId(),
     title: 'Timed event',
-    start: todayStr + 'T12:00:00'
+    start: TODAY_STR + 'T12:00:00'
   }
 ];
 
