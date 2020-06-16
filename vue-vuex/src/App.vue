694f73c8e57a4938cc6a2227c49c755434a8135c
Paul Young <paul-young@live.co.uk>
2020-06-16 13:45:29 +0100

    remove boilerplate bits

--- vue-vuex/src/App.vue
+++ vue-vuex/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FullCalendar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FullCalendar from '@fullcalendar/vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    FullCalendar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+          font: 14px sans-serif;
+    }
 </style>
