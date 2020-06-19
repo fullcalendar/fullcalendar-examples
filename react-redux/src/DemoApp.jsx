d31ce679fef55a12395ddfb2bf003eb8c97db9cd
Adam Shaw <arshaw@users.noreply.github.com>
2020-06-19 01:26:09 -0400

    remove unnecessary createRefs

--- react-redux/src/DemoApp.jsx
+++ react-redux/src/DemoApp.jsx
@@ -1,4 +1,4 @@
-import React, { createRef } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { createSelector } from 'reselect'
 import FullCalendar, { formatDate } from '@fullcalendar/react'
--- react-typescript/src/DemoApp.tsx
+++ react-typescript/src/DemoApp.tsx
@@ -1,4 +1,4 @@
-import React, { createRef } from 'react'
+import React from 'react'
 import FullCalendar, { EventApi, DateSelectArg, EventClickArg, EventContentArg, formatDate } from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
