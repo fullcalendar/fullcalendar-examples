9dbfb8c703d39b6206117b17c33423be6b368c61
Adam Shaw <arshaw@users.noreply.github.com>
2020-06-19 12:06:27 -0400

    bootstrap example

---
+++ bootstrap/README.md
@@ -0,0 +1,26 @@
+
+# FullCalendar Bootstrap Example
+
+This is a fully-buildable example project for FullCalendar and [Bootstrap].
+
+
+## Installation
+
+```bash
+git clone https://github.com/fullcalendar/fullcalendar-example-projects.git
+cd fullcalendar-example-projects/bootstrap
+npm install
+```
+
+## Build Commands
+
+```bash
+npm run build
+npm run watch # continously build
+npm run clean # start fresh
+```
+
+After running `build` or `watch`, open up `index.html` in a browser.
+
+
+[Bootstrap]: https://getbootstrap.com/
---
+++ bootstrap/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'sourcemap',
+  entry: './src/main.js',
+  resolve: {
+    extensions: [ '.js' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader', options: { importLoaders: 1 } }
+        ]
+      },
+
+      // make sure raw font files referenced from the fontawesome stylesheet are copied into dist
+      // from https://chriscourses.com/blog/loading-fonts-webpack
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ]
+}
