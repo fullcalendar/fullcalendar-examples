74729d1f897e420e1aeb24d51ea3ca5c815c7c56
Adam Shaw <arshaw@users.noreply.github.com>
2020-06-19 18:26:14 -0400

    simple moment example

---
+++ moment/README.md
@@ -0,0 +1,28 @@
+
+# FullCalendar Moment Example Project
+
+This is a fully-buildable example project for FullCalendar and [Moment], leveraging [Webpack].
+
+
+## Installation
+
+```bash
+git clone https://github.com/fullcalendar/fullcalendar-example-projects.git
+cd fullcalendar-example-projects/moment
+npm install
+```
+
+## Build Commands
+
+```bash
+npm run build
+npm run watch # continously build
+npm run clean # start fresh
+```
+
+After running `build` or `watch`, open up `index.html` in a browser.
+
+
+[Moment]: https://momentjs.com/
+[Webpack]: https://webpack.js.org/
+[ts-loader]: https://github.com/TypeStrong/ts-loader
---
+++ moment/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'sourcemap',
+  entry: './src/main.js',
+  resolve: {
+    extensions: [ '.js' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader', options: { importLoaders: 1 } }
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new MomentLocalesPlugin(), // strip all locales except 'en'
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ]
+}
