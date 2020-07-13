720214fd72748c8db9a11dac4cbdc4e1d87b8bed
Adam Shaw <arshaw@users.noreply.github.com>
2020-07-12 21:16:33 -0400

    barbones nuxt

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    this is a test
+  </div>
+</template>
+
+<script>
+// import FullCalendar from "@fullcalendar/vue";
+
+export default {
+  components: {
+    // FullCalendar
+  }
+};
+</script>
