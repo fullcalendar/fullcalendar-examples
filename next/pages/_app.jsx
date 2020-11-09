a44c3e1f4f5079b23d57c18602a14ea26ca95530
Michael Lancaster <michaell.llancaster@gmail.com>
2020-11-09 13:26:19 -0800

    move fc css to page for scoped styling

--- next/pages/_app.jsx
+++ next/pages/_app.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import '@fullcalendar/common/main.css'
-import '@fullcalendar/timegrid/main.css'
 import '../global-styles.css'
 
 export default function App({ Component, pageProps }) {
