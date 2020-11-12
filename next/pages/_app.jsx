bc7ba6c1a3873022f52bbad83fd1d0fb47404eb8
Adam Shaw <arshaw@users.noreply.github.com>
2020-11-12 00:19:17 -0500

    css problem in next.js demo, with daygrid

--- next/pages/_app.jsx
+++ next/pages/_app.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import '@fullcalendar/common/main.css'
+import '@fullcalendar/daygrid/main.css'
 import '@fullcalendar/timegrid/main.css'
 import '../global-styles.css'
 
