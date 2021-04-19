2e21191d341d2595691e9062e9445862a5e63df6
Naguumo <ishbharal@gmail.com>
2021-04-19 13:39:29 -0500

    base changes

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,6 +1,11 @@
+import PnpWebpackPlugin from "pnp-webpack-plugin";
 
 module.exports = {
   build: {
-    transpile: /@fullcalendar.*/ // transpile ESM modules within all fullcalendar packages
+    transpile: /@fullcalendar.*/,
+    extend(config) {
+      config.resolve.plugins.push(PnpWebpackPlugin);
+      config.resolveLoader.plugins.push(PnpWebpackPlugin.moduleLoader(module));
+    }
   }
-}
+};
--- vue-typescript/vue.config.js
+++ vue-typescript/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: './' // for the `build` command
-}
+  publicPath: "./", // for the `build` command
+};
