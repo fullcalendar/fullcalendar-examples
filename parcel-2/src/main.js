f6690f91334d6a9e751cdfefe13a072560fab69a
Naguumo <ishbharal@gmail.com>
2021-04-22 19:15:36 -0500

    parcel-2

--- parcel-2/src/main.js
+++ parcel-2/src/main.js
@@ -1,19 +1,19 @@
-import { Calendar } from '@fullcalendar/core';
-import interactionPlugin from '@fullcalendar/interaction';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import listPlugin from '@fullcalendar/list';
-import './main.css';
+import { Calendar } from '@fullcalendar/core'
+import interactionPlugin from '@fullcalendar/interaction'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import listPlugin from '@fullcalendar/list'
+import './main.css'
 
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
+document.addEventListener('DOMContentLoaded', function () {
+  const calendarEl = document.getElementById('calendar')
 
-  var calendar = new Calendar(calendarEl, {
-    plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin ],
+  const calendar = new Calendar(calendarEl, {
+    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
     },
     initialDate: '2018-01-12',
     navLinks: true, // can click day/week names to navigate views
@@ -27,55 +27,55 @@ document.addEventListener('DOMContentLoaded', function() {
       {
         title: 'Long Event',
         start: '2018-01-07',
-        end: '2018-01-10'
+        end: '2018-01-10',
       },
       {
         groupId: 999,
         title: 'Repeating Event',
-        start: '2018-01-09T16:00:00'
+        start: '2018-01-09T16:00:00',
       },
       {
         groupId: 999,
         title: 'Repeating Event',
-        start: '2018-01-16T16:00:00'
+        start: '2018-01-16T16:00:00',
       },
       {
         title: 'Conference',
         start: '2018-01-11',
-        end: '2018-01-13'
+        end: '2018-01-13',
       },
       {
         title: 'Meeting',
         start: '2018-01-12T10:30:00',
-        end: '2018-01-12T12:30:00'
+        end: '2018-01-12T12:30:00',
       },
       {
         title: 'Lunch',
-        start: '2018-01-12T12:00:00'
+        start: '2018-01-12T12:00:00',
       },
       {
         title: 'Meeting',
-        start: '2018-01-12T14:30:00'
+        start: '2018-01-12T14:30:00',
       },
       {
         title: 'Happy Hour',
-        start: '2018-01-12T17:30:00'
+        start: '2018-01-12T17:30:00',
       },
       {
         title: 'Dinner',
-        start: '2018-01-12T20:00:00'
+        start: '2018-01-12T20:00:00',
       },
       {
         title: 'Birthday Party',
-        start: '2018-01-13T07:00:00'
+        start: '2018-01-13T07:00:00',
       },
       {
         title: 'Click for Google',
         url: 'http://google.com/',
-        start: '2018-01-28'
-      }
-    ]
-  });
+        start: '2018-01-28',
+      },
+    ],
+  })
 
-  calendar.render();
-});
+  calendar.render()
+})
