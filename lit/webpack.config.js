e3c555c6eb8d601d4c1280f3ad27a1c5e2104fcb
Adam Shaw <arshaw@users.noreply.github.com>
2021-07-27 16:08:09 -0400

    switch lit example to using webpack

---
+++ lit/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/main.js',
+  resolve: {
+    extensions: [ '.js' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader', options: { importLoaders: 1 } }
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ]
+}
