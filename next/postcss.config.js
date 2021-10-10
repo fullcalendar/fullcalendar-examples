b967f5e058bf2dd8e3f1cc51438c4318de1413b9
Chang Wei <changwei1006@gmail.com>
2021-10-10 20:32:48 +0800

    fix the comment typo
    fix the comment typo in ./edit/master/next/postcss.config.js

--- next/postcss.config.js
+++ next/postcss.config.js
@@ -1,5 +1,5 @@
 
-// this filee is only needed for fullcalendar's weird monorepo setup. delete in your own project
+// this file is only needed for fullcalendar's weird monorepo setup. delete in your own project
 
 module.exports = {
   plugins: {}
