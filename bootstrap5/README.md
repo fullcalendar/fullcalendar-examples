3476139c8b74b1f1b50f8225fb85d1b34eeaa064
Adam Shaw <arshaw@users.noreply.github.com>
2022-02-09 17:15:30 -0500

    bootstrap5 demo

---
+++ bootstrap5/README.md
@@ -0,0 +1,33 @@
+
+# FullCalendar Bootstrap Example
+
+This is a fully-buildable example project for FullCalendar and [Bootstrap].
+
+
+## Installation
+
+```bash
+git clone https://github.com/fullcalendar/fullcalendar-example-projects.git
+cd fullcalendar-example-projects/bootstrap
+npm install
+```
+
+## Build Commands
+
+```bash
+npm run build
+npm run watch # continously build
+npm run clean # start fresh
+```
+
+After running `build` or `watch`, open up `index.html` in a browser.
+
+
+## A Note on Dependencies
+
+This project's dependencies specify `jquery` and `popper.js`. However, they are not used.
+They are only included to satisfy the `bootstrap` package's peerDependency requirement,
+which prevents an error from displaying.
+
+
+[Bootstrap]: https://getbootstrap.com/
---
+++ bootstrap5/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+// Try the environment variable, otherwise use root
+const ASSET_PATH = process.env.ASSET_PATH || './'
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/main.js',
+  resolve: {
+    extensions: [ '.js' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader', options: { importLoaders: 1 } }
+        ]
+      },
+
+      // make sure raw font files referenced from the fontawesome stylesheet are copied into dist
+      // from https://chriscourses.com/blog/loading-fonts-webpack
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: ASSET_PATH
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ]
+}
