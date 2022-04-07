ea2034420d8d7c077bc5e5614020377ab90edfb6
Adam Shaw <arshaw@users.noreply.github.com>
2022-04-07 14:26:53 -0400

    react lazy example

---
+++ react-lazy/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const packageMeta = require('./package.json')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/main.jsx',
+  resolve: {
+    extensions: [ '.js', '.jsx' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader' // will use .babelrc
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
+  output: {
+    filename: 'main.js',
+    path: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: packageMeta.title
+    })
+  ]
+}
