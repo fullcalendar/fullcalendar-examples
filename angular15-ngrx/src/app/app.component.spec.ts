6dab28d2d6724c51b858380a833ed3fa5e4e00a9
Adam Shaw <arshaw@users.noreply.github.com>
2022-12-13 21:58:27 -0500

    make ngrx test pass

--- angular15-ngrx/src/app/app.component.spec.ts
+++ angular15-ngrx/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      imports: [
+        AppModule
       ],
     }).compileComponents();
   });
