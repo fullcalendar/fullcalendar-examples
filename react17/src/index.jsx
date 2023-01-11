cf6803da5817af572d9d2af5855ab621e2dc1af7
Adam Shaw <arshaw@users.noreply.github.com>
2023-01-10 20:01:14 -0500

    react 17 render method

--- react17/src/index.jsx
+++ react17/src/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import { render } from 'react-dom'
 import DemoApp from './DemoApp'
 import './index.css'
 
 document.addEventListener('DOMContentLoaded', function() {
-  createRoot(document.body.appendChild(document.createElement('div')))
-    .render(<DemoApp />)
+  render(
+    <DemoApp />,
+    document.body.appendChild(document.createElement('div'))
+  )
 })
