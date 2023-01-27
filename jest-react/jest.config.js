d36e038f2ea51196756b5212733a7d6bedf79f56
Adam Shaw <arshaw@users.noreply.github.com>
2023-01-27 17:57:42 -0500

    jest workaround and readme

--- jest-react/jest.config.js
+++ jest-react/jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js']
+
+  // workaround for  "Unexpected token 'exports'" error while parsing Preact
+  testEnvironmentOptions: {
+    customExportConditions: [] // don't load "browser" field
+  }
 }
