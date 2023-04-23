7f6929fde01369f6af2ffb4f21a7802d7d47855d
Adam Shaw <arshaw@users.noreply.github.com>
2023-04-23 00:18:01 -0400

    start using FullCalendar in Remix example

--- remix/app/routes/index.tsx
+++ remix/app/routes/index.tsx
@@ -1,8 +1,24 @@
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 export default function Index() {
   return (
     <div className="index-route">
-      This is a test
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }}
+        initialView='dayGridMonth'
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+      />
     </div>
   );
 }
