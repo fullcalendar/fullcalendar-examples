2ed27d3f5cb507d908b516f697ce5572cce8a6e2
Adam Shaw <arshaw@users.noreply.github.com>
2023-11-28 18:59:58 -0500

    fix monorepo-hack script

--- angular17/monorepo-hack.cjs
+++ angular17/monorepo-hack.cjs
@@ -2,26 +2,27 @@
 //
 // Executes a workaround for monorepos. If not in a monorepo, nothing will happen.
 //
+// Inlines the @fullcalendar/angular symlink. Prevents the symlink from accessing
+// its peer dependencies and having two Angular versions, leading to errors.
+// This is the fault of Angular custom dependency-resolution logic.
+//
 
 const fs = require('fs/promises')
 
-//
-// Inline the symbolic link. Prevents the symlinked @fullcalendar/angular from acessing
-// its peer dependencies and having two Angulars and leading to errors.
-// This is the fault of Angular custom dependency-resolution logic.
-//
-copyDirIfSymlink('./node_modules/@fullcalendar/angular').catch((error) => {
+runMonorepoHack('./node_modules/@fullcalendar/angular').catch((error) => {
   console.error(error.message)
   process.exit(1)
 })
 
-async function copyDirIfSymlink(sourcePath) {
-  const stats = await fs.lstat(sourcePath)
+async function runMonorepoHack(depPath) {
+  const symlinkPath = `${depPath}_orig`
 
-  if (stats.isSymbolicLink()) {
-    const realPath = await fs.realpath(sourcePath)
+  await fs.lstat(symlinkPath).catch(() => {
+    return fs.rename(depPath, symlinkPath)
+  })
 
-    await fs.unlink(sourcePath)
-    await fs.cp(realPath, sourcePath, { recursive: true })
-  }
+  const sourcePath = await fs.realpath(symlinkPath)
+
+  await fs.rm(depPath, { recursive: true, force: true })
+  await fs.cp(sourcePath, depPath, { recursive: true })
 }
